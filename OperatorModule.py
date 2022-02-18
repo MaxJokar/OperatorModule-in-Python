495f5863fa6e729d5eebdaa846d79ba5c2665cb9
email <mmb2020max@gmail.com>
2022-02-18 13:48:03 +0200

    add sub mul mod truediv flrdiv  Done

---
+++ OperatorModule.py
@@ -0,0 +1,14 @@
+import operator
+# print(dir(operator))
+
+print(operator.add(17,9))
+print(operator.sub(17,9))
+print(operator.mul(17,9))
+print(operator.truediv(17,9))
+print(operator.floordiv(17,9))
+print(operator.mod(17,9))
+
+
+
+
+
